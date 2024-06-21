182054469a50a0492a147749f488cff095e9a4d3
+ <nickson.mogaka@student.moringaschool.com>
2024-06-21 11:02:55 +0300

    modify

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";  // Change name to "Susan"
+const height = 35;     // Change height to a value less than 40 and greater than 0
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
